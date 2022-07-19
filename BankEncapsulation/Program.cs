b7af3761d99f6fda4dee44054966a975094271bd
Jayden Callan <jqcallan04@gmail.com>
2022-07-19 13:38:23 -0600

    finished exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -7,7 +7,26 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
+            var account = new BankAccount();
 
+            string input = "";
+            bool deposited = false;
+            do
+            {
+                Console.WriteLine("How much do you want to deposit?");
+                try
+                {
+                    input = Console.ReadLine();
+                    account.Deposit(double.Parse(input));
+                    deposited = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid number");
+                }
+            } while (deposited == false);
+
+            Console.WriteLine($"You have ${account.GetBalance()} in your account");
         }
     }
 }
